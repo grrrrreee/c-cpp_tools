2b4f63eb95cb1292fa36ef6a04f6cd98eb2483cc
finchparker <parkms940@naver.com>
2019-12-25 00:29:00 +0900

    2019_12_25_00_28

--- cpp_tools/application/8.cpp
+++ cpp_tools/application/8.cpp
@@ -23,30 +23,26 @@ int separate::stage1(int a) {
     return num;
 }
 
-int separate::stage2(int a, int b){
+int separate::stage2(int a, int b) {
     separate sp;
     b = sp.stage1(a);
-    while(b != 0) {
-        int c,d;
-        c = pow(10,b-1);
-        d = a % c;
-        a = (a - d) / c;
-        // 1000, 200, 30, 4
-        // a = (a - d);
-        b--;
-        cout << a <<endl;
-        a = d;
+    int n; n = 0;
+    while(n < b) {
+        int c;
+        c = a % 10;
+        a = a - c;
+        a = a/10;
+        n ++;
+        cout << c <<endl;
     }
-    return a;
+    return 0;
 }
 
 int main() {
     separate sp;
     int a; cout << "a is : "; cin >> a;
-    sp.stage1(a);
-    // 1234
-    // 1, 2, 3, 4
+    
     int b;
-    b= sp.stage1(a);
+    b = sp.stage1(a);
     sp.stage2(a, b);
 }
